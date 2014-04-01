e093075d5e66d8f35ad323759352d09220f06e1c
John Lee <drumwolfmail@yahoo.com>
2014-03-31 20:45:02 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videoshare::Application.config.secret_key_base = 'b5eeb2f011d76b1a42812e068f88dcad54ae2cb404242bd1630303cdbb23d09ae1e4e4c92e9b61178deb251194fe4fee8ebdf1ee205e854635688cc85ff5a576'
